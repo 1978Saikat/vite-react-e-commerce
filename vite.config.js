cec1b267c03ada633656a6e2e1b8888fd419d50a
saikat ghosh <saikat.ghosh@htdigital.in>
2025-07-23 22:05:10 +0530

    2nd commit

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/vite-react-e-commerce/', // Replace with your GitHub repo name
   plugins: [react()],
 })
